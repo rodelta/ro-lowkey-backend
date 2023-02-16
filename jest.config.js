4fa8b725cb2e62693b2de00bc39783abe353804c
Ro De la Rivera <crdelarivera@gmail.com>
2023-02-16 20:28:04 +0100

    move to rust-based jest runner

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-    testEnvironment: "node",
-    collectCoverage: true,
-    collectCoverageFrom: [
-        "**/*.ts",
-        "!**/*.test.ts",
-        "!**/index.ts",
-        "!**/node_modules/**",
-    ],
-    roots: ["<rootDir>/src"],
-    moduleFileExtensions: ["js", "ts"],
-    preset: "ts-jest",
-    testMatch: ["<rootDir>/src/**/*.test.ts"],
+	testEnvironment: "node",
+	collectCoverage: true,
+	collectCoverageFrom: [
+		"**/*.ts",
+		"!**/*.test.ts",
+		"!**/index.ts",
+		"!**/node_modules/**",
+	],
+	roots: ["<rootDir>/src"],
+	moduleFileExtensions: ["js", "ts"],
+	testMatch: ["<rootDir>/src/**/*.test.ts"],
+	transform: {
+		"^.+\\.ts?$": "@swc/jest",
+	},
 };
