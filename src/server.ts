5247f1a8905b8f1fee32267eb14740d7e7923e16
Rodrigo De la Rivera <crdelarivera@gmail.com>
2025-04-22 03:15:38 +0200

    initial server load message with env node

--- src/server.ts
+++ src/server.ts
@@ -8,25 +8,26 @@ import { performance } from "perf_hooks";
  */
 
 if (!process.env.NODE_ENV)
-	throw new Error("Cannot read NODE_ENV. Is your .env file present?");
+  throw new Error("Cannot read NODE_ENV. Is your .env file present?");
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 8880;
 const HOST = process.env.HOST ?? "http://localhost";
 const isDev = process.env.NODE_ENV === "development";
 
 const startServer = async () => {
-	const startMs = performance.now();
+  console.log(`** Starting server in mode ${process.env.NODE_ENV}`);
+  const startMs = performance.now();
 
-	(await import("./errors")).setProcessExceptions();
-	if (isDev) (await import("./errors")).prettifyErrors();
+  (await import("./errors")).setProcessExceptions();
+  if (isDev) (await import("./errors")).prettifyErrors();
 
-	const app = (await import("./app")).loadApp();
-	const server = http.createServer(app);
+  const app = (await import("./app")).loadApp();
+  const server = http.createServer(app);
 
-	server.listen(PORT, () => {
-		const executionMs = (performance.now() - startMs).toFixed(1);
-		console.log(`** Server started on ${HOST}:${PORT} (${executionMs} ms)`);
-	});
+  server.listen(PORT, () => {
+    const executionMs = (performance.now() - startMs).toFixed(1);
+    console.log(`** Server started on ${HOST}:${PORT} (${executionMs} ms)`);
+  });
 };
 
 startServer();
